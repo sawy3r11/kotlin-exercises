6e5df03e23efc1d7748ebd4f0dc8607a0c23ce22
dawid <sawy3r11@gmail.com>
2025-10-20 15:56:18 +0200

    money impl

--- src/main/kotlin/essentials/operators/Money.kt
+++ src/main/kotlin/essentials/operators/Money.kt
@@ -14,77 +14,99 @@ data class Money(
         fun eur(amount: String) =
             Money(BigDecimal(amount), Currency.EUR)
     }
+
+    operator fun minus(other: Money)=
+        when {
+            this.currency != other.currency -> {
+                throw IllegalArgumentException("Cannot subtract money with different currencies: ${this.currency} and ${other.currency}")
+            }
+            else -> Money(this.amount - other.amount, this.currency)
+        }
+
+    operator fun unaryMinus() = Money(-this.amount, this.currency)
+
+    operator fun times(factor: Int) = Money(this.amount * BigDecimal(factor), this.currency)
 }
+
+operator fun Money.plus(other: Money): Money =
+    when {
+        this.currency != other.currency -> {
+            throw IllegalArgumentException("Cannot add money with different currencies: ${this.currency} and ${other.currency}")
+        }
+
+        else -> Money(this.amount + other.amount, this.currency)
+    }
+
 
 enum class Currency {
     EUR, USD, GBP
 }
 
 fun main() {
-    //val money1 = Money.eur("10.00")
-    //val money2 = Money.eur("29.99")
+    val money1 = Money.eur("10.00")
+    val money2 = Money.eur("29.99")
     //
-    //println(money1 + money2) // Money(amount=39.99, currency=EUR)
-    //println(money2 - money1) // Money(amount=19.99, currency=EUR)
-    //println(-money1) // Money(amount=-10.00, currency=EUR)
-    //println(money1 * 3) // Money(amount=30.00, currency=EUR)
+    println(money1 + money2) // Money(amount=39.99, currency=EUR)
+    println(money2 - money1) // Money(amount=19.99, currency=EUR)
+    println(-money1) // Money(amount=-10.00, currency=EUR)
+    println(money1 * 3) // Money(amount=30.00, currency=EUR)
 }
 
 class MoneyOperationsTest {
-//    @Test
-//    fun `Money can be added`() {
-//        val money1 = Money.eur("10.00")
-//        val money2 = Money.eur("29.99")
-//        assertEquals(Money.eur("39.99"), money1 + money2)
-//    }
-//
-//    @Test
-//    fun `Money throws exception when adding different currencies`() {
-//        for (currency in Currency.entries) {
-//            for (otherCurrency in (Currency.entries - currency)) {
-//                val money1 = Money(BigDecimal("10.00"), currency)
-//                val money2 = Money(BigDecimal("29.99"), otherCurrency)
-//                assertThrows<IllegalArgumentException> { money1 + money2 }
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `Money can be subtracted`() {
-//        val money1 = Money.eur("10.00")
-//        val money2 = Money.eur("29.99")
-//        assertEquals(Money.eur("19.99"), money2 - money1)
-//    }
-//
-//    @Test
-//    fun `Money throws exception when subtracting different currencies`() {
-//        for (currency in Currency.entries) {
-//            for (otherCurrency in (Currency.entries - currency)) {
-//                val money1 = Money(BigDecimal("10.00"), currency)
-//                val money2 = Money(BigDecimal("29.99"), otherCurrency)
-//                assertThrows<IllegalArgumentException> { money1 - money2 }
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun `Money can be negated`() {
-//        assertEquals(Money.eur("-10.00"), -Money.eur("10.00"))
-//        assertEquals(Money.eur("-100.00"), -Money.eur("100.00"))
-//        assertEquals(Money.eur("10.00"), -Money.eur("-10.00"))
-//        assertEquals(Money.eur("-999.999"), -Money.eur("999.999"))
-//
-//    }
-//
-//    @Test
-//    fun `Money can be multiplied by a number`() {
-//        assertEquals(Money.eur("30.00"), Money.eur("10.00") * 3)
-//        assertEquals(Money.eur("0.00"), Money.eur("10.00") * 0)
-//        assertEquals(Money.eur("-30.00"), Money.eur("10.00") * -3)
-//    }
+    @Test
+    fun `Money can be added`() {
+        val money1 = Money.eur("10.00")
+        val money2 = Money.eur("29.99")
+        assertEquals(Money.eur("39.99"), money1 + money2)
+    }
+
+    @Test
+    fun `Money throws exception when adding different currencies`() {
+        for (currency in Currency.entries) {
+            for (otherCurrency in (Currency.entries - currency)) {
+                val money1 = Money(BigDecimal("10.00"), currency)
+                val money2 = Money(BigDecimal("29.99"), otherCurrency)
+                assertThrows<IllegalArgumentException> { money1 + money2 }
+            }
+        }
+    }
+
+    @Test
+    fun `Money can be subtracted`() {
+        val money1 = Money.eur("10.00")
+        val money2 = Money.eur("29.99")
+        assertEquals(Money.eur("19.99"), money2 - money1)
+    }
+
+    @Test
+    fun `Money throws exception when subtracting different currencies`() {
+        for (currency in Currency.entries) {
+            for (otherCurrency in (Currency.entries - currency)) {
+                val money1 = Money(BigDecimal("10.00"), currency)
+                val money2 = Money(BigDecimal("29.99"), otherCurrency)
+                assertThrows<IllegalArgumentException> { money1 - money2 }
+            }
+        }
+    }
+
+    @Test
+    fun `Money can be negated`() {
+        assertEquals(Money.eur("-10.00"), -Money.eur("10.00"))
+        assertEquals(Money.eur("-100.00"), -Money.eur("100.00"))
+        assertEquals(Money.eur("10.00"), -Money.eur("-10.00"))
+        assertEquals(Money.eur("-999.999"), -Money.eur("999.999"))
+
+    }
+
+    @Test
+    fun `Money can be multiplied by a number`() {
+        assertEquals(Money.eur("30.00"), Money.eur("10.00") * 3)
+        assertEquals(Money.eur("0.00"), Money.eur("10.00") * 0)
+        assertEquals(Money.eur("-30.00"), Money.eur("10.00") * -3)
+    }
 }
 
-inline fun <reified T: Throwable> assertThrows(operation: () -> Unit) {
+inline fun <reified T : Throwable> assertThrows(operation: () -> Unit) {
     val result = runCatching { operation() }
     assert(result.isFailure) { "Operation has not failed with exception" }
     val exception = result.exceptionOrNull()
